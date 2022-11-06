018c4535ad516d62f45e6fa9dfe1d35f8bb4c9ae
lacey aguigam <laceyaguigam@yahoo.com>
2022-11-05 21:48:03 -0700

    added intern to index.js

--- index.js
+++ index.js
@@ -148,19 +148,61 @@ function appMenu() {
             }
         }
 
-    // ]).then(answers => {
-    //     const engineer = add Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
-    //     teamMembers.push(engineer);
-    //     idArray.push(answers.engineerId);
-    //     createTeam();
-    // });
-    }
+    ]).then(answers => {
+            const engineer = add Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+            teamMembers.push(engineer);
+            idArray.push(answers.engineerId);
+            createTeam();
+        });
+        }
 
     function addIntern(){
         inquirer.prompt([
-
-        ])
-    }
+            type: "input",
+            name: "internName",
+            message: "What is the Intern's name?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter at least one character.";
+            }
+        },
+        {
+            type: "input",
+            name: "internId",
+            message: "What is the interns's ID?",
+            validate: answer => {
+                const pass = answer.match(/^[1-9]\d*$/);
+                if (pass) {
+                    return true;
+                }
+                return "Please enter a positive number greater than zero.";
+            }
+        },
+        {
+            type: "input",
+            name: "internEmail",
+            message: "What is the intern's email?",
+            validate: answer => {
+                const pass = answer.match(/\$+@\S+\.\S+/);
+                if (pass) {
+                    return true;
+                }
+                return "Please enter a valid email address.";
+            }
+        },
+        {
+            type: "input",
+            name: "internSchool",
+            message: "What is the intern's school?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter at least one character.";
+            }
+        }
 
     function buildTeam(){
         // create the output directory if the output path doesnt exist
@@ -175,37 +217,5 @@ function appMenu() {
 
 appMenu();
 
-
-
-// WHEN I am prompted for my team members and their information
-//THEN an HTML file is generated that displays a nicely formatted team roster based on user input
-
-
-// WHEN I click on an email address in the HTML
-// THEN my default email program opens and populates the TO field of the email with the address
-
-
-// WHEN I click on the GitHub username
-// THEN that GitHub profile opens in a new tab
-
-
-// WHEN I start the application
-// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-
-
-// WHEN I enter the team manager’s name, employee ID, email address, and office number
-// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
-
-
-// WHEN I select the engineer option
-// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
-
-
-// WHEN I select the intern option
-// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
-
-
-// WHEN I decide to finish building my team
-// THEN I exit the application, and the HTML is generated
 
 
